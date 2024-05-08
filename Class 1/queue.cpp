d6c1388570f82fddaac2756d31e52e7437d94e42
Kamrul785 <133371323+Kamrul785@users.noreply.github.com>
2024-05-08 19:54:50 +0600

    Add files via upload
    Class 1 updated

---
+++ Class 1/queue.cpp
@@ -0,0 +1,50 @@
+
+// printing sortest path of the graph
+
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+#define fl float
+using namespace std;
+vector<int> v[1005];
+bool vis[1005];
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        cout << par << endl;
+        for (int child : v[par])
+        {
+            if (vis[child] == false)
+            {
+                q.push(child);
+                vis[child] = true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    int src;
+    cin >> src;
+    memset(vis, false, sizeof(vis));
+    bfs(src);
+    return 0;
+}
